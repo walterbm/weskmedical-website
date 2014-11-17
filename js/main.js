9b7aa70afa1e05f6f0c193179ffef2127eae8594
WalterBM <walter.beller.morales@gmail.com>
2014-11-17 01:09:15 -0500

    Added Navigation
    Added desktop navigation and smooth scrolling. Preparing for mobile
    navigation.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
+$(document).ready(function(){
 
+	$("#hamburger").click(function(){
+		$(this).toggleClass('open');
+	});
+
+	
+	$(function() {
+	  $('a[href*=#]:not([href=#])').click(function() {
+	    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+	      var target = $(this.hash);
+	      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+	      if (target.length) {
+	        $('html,body').animate({
+	          scrollTop: target.offset().top
+	        }, 1000);
+	        return false;
+	      }
+	    }
+	  });
+	});
+
+});
